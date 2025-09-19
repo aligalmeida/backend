38e093ca0d7902125765e9fb54068f42169089c9
Aline <aligalmeida@hotmail.com>
2025-09-18 21:47:19 -0300

    aula06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-
+const indexRouter = require("./routes/index");
+const tarefaRouter = require("./routes/tarefas");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-app.use('/', indexRouter);
-
+app.use("/", indexRouter);
+app.use("/tarefas", tarefaRouter);
 
 module.exports = app;
